243c2dd1765c4dc5743f6926abf772599c395101
Sittipong Hemloun <77152671+ong22280@users.noreply.github.com>
2022-11-11 21:03:12 +0700

    init font awesome

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import App from './App.vue'
-import router from './router'
+import "./assets/main.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fab);
+import { far } from "@fortawesome/free-regular-svg-icons";
+library.add(far);
 
-import './assets/main.css'
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
 
-const app = createApp(App)
+app.use(router);
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
